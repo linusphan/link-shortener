df10cf8ff16308fbdfb91a019bbe4ca107b6b83f
Linus Phan <phanlinus@gmail.com>
2019-06-03 23:19:17 -0700

    update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'short_links#new'
 
+  get 's/:shortened_url_key', to: 'short_links#index'
+
   resources :short_links, only: [:new, :create, :show, :update]
-  get 's/:short_url', to: 'short_links#index'
 end
